44c14dfda99c5c4e39e54af418325843938810b2
Peter Cai <peter@typeblog.net>
2019-10-23 19:52:55 +0800

    i9: overlay: enable higherAspectRatioScreen
    Change-Id: I992036349ec0bebf365155f41df236547d87dc24

--- device.mk
+++ device.mk
@@ -22,7 +22,8 @@ $(call inherit-product-if-exists, vendor/ktouch/i9/i9-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # A/B
 AB_OTA_UPDATER := false
