4accc71d30ce1a4cc4cbbd2d3e9c28a28b09b047
Peter Cai <peter@typeblog.net>
2019-10-25 06:58:51 +0800

    i9: enable secure_lock_screen feature
    Change-Id: Ieb2544904b2ce5ddab28e72804e841ab5247623b

--- device.mk
+++ device.mk
@@ -47,6 +47,10 @@ PRODUCT_PACKAGES += \
     init.wifi.mt6739.sh \
     fstab.enableswap
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.secure_lock_screen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.secure_lock_screen.xml
+
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext
