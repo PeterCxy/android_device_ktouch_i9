147c0fd7fbbc370c84e6e1eadc2351f7f1490cd1
Jesse Chan <jc@lineageos.org>
2019-10-24 17:34:01 +0800

    i9: remove duplicate fuseblk define from platform
    * This way we don't need to hack init or device/lineage/sepolicy
    
    Change-Id: I74eb0172c5d89d45d63fe1fa831d89648703ff38

---
+++ releasetools/releasetools.py
@@ -0,0 +1,4 @@
+def FullOTA_InstallEnd(info):
+    info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/platform/bootdevice/by-name/system", "/system_root");')
+    info.script.AppendExtra('run_program("/sbin/sed", "-i", "/fuseblk/d", "/system_root/system/etc/selinux/plat_sepolicy.cil");')
+    info.script.AppendExtra('unmount("/system_root");')
