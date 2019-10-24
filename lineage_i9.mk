3333a4ba506fda1d108fc8d70d41a024a40c2379
Peter Cai <peter@typeblog.net>
2019-10-25 06:52:42 +0800

    i9: enable avoiding gfx acceleration again
    * Something is still broken in the graphics stack. The rendering is much
      slower than it was on Android 9 anyway, so let's disable it for now.
    * At least now we don't need the dirty hack in frameworks in any sense.
    
    Change-Id: I8fa0552efdfd2a50c292491b11f60c829b466200

--- lineage_i9.mk
+++ lineage_i9.mk
@@ -33,6 +33,7 @@ TARGET_SCREEN_WIDTH := 340
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vndk.version=27 \
     ro.skia.ignore_swizzle=true \
+    ro.config.avoid_gfx_accel=true \
     ro.radio.apn_force_cognitive=true
 
 # GMS
